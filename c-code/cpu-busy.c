66d0f50d3f7ec3fbbac4ec794386dece04064483
Meng Xu <mengxu@cis.upenn.edu>
2015-01-03 23:25:51 -0500

    xen: domU: script to create a domU image
    The file install-vm-from-iso.xl is the correct script to create a domU
    from an Ubuntu ISO. We do not need to tackle the troublesome network to
    install a domU then.
    	modified:   c-code/cpu-busy.c
    	modified:   c-code/typesize
    	modified:   c-code/typesize.c
    	new file:   xen/guestU/guest1.xl
    	new file:   xen/guestU/guest1.xl.2
    	new file:   xen/guestU/guest1.xl.backup
    	new file:   xen/guestU/install-vm-from-iso.xl
    	new file:   xen/guestU/note.txt

--- c-code/cpu-busy.c
+++ c-code/cpu-busy.c
@@ -3,10 +3,12 @@
 void main()
 {
     int i=0;
-    double result;
+    double result = 0;
     for(i=0;; i++)
     {
         result += ((i+1)*i)%(i+2);
+	if (result >= 100000000)
+		result = 1;
     }
 
 }
